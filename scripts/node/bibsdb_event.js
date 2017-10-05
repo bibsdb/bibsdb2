5089e8e70c929a143acda94d4eadc6ef6e80d2de
Agnete Moos <agms@sonderborg.dk>
2017-10-05 12:54:37 +0200

    Initial commit

---
+++ scripts/node/bibsdb_event.js
@@ -0,0 +1,12 @@
+(function($) {
+  'use strict';
+
+	 // Translate target label on event full
+  Drupal.behaviors.ding_event_translate_target_label_on_event_full = {
+    attach: function(context, settings) {
+    	$( ".node-ding-event.node-full .field-name-field-ding-event-target .field-label:contains('Target')" , context).text("Adgang");   
+    }
+  };
+	
+
+})(jQuery);
