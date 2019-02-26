6749bb38552dbdddb9a4436da264564d31fd4720
Agnete Moos <agms@sonderborg.dk>
2019-02-26 10:41:33 +0000

    Make holdings-info start expanded on page-load

--- scripts/bibsdb2.common.js
+++ scripts/bibsdb2.common.js
@@ -10,14 +10,20 @@
     }
   };
 
+  /*
   // Hide Internal problem warning message
   Drupal.behaviors.hide_internal_problem = {
     attach: function(context, settings) {
       $(".warning:contains('Internal problem')").hide();
     }
   };
+  */
 
-
-
+   // Make holdings-info start expanded on page-load
+  Drupal.behaviors.expand_holdings_on_page_load = {
+    attach: function(context, settings) {
+      $('.group-holdings-available .field-group-format-wrapper').css("display", "block"); 
+    }
+  };
 
 })(jQuery);
