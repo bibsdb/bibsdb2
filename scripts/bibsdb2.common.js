53a32e0b916d27e2de35ad3f7e94916d7452a91a
Agnete Moos <agms@sonderborg.dk>
2019-06-12 11:49:40 +0000

    Fix js-error that hides lead and body

--- scripts/bibsdb2.common.js
+++ scripts/bibsdb2.common.js
@@ -21,16 +21,11 @@
    // Nodelist - Node blocks. Hide field-type-text-long when the page is initially loaded.
   Drupal.behaviors.nodelist_node_blocks_hide_teaser = {
     attach: function(context, settings) {
-      $('.field-type-text-long').toggleClass('element-hidden', true);
+      $('.ding_nodelist-node_blocks .nb-item .text .title-and-lead .field-type-text-long').toggleClass('element-hidden', true);
     }
   };
 
-  // Nodelist - Node blocks. Hide field-type-text-long when the page is initially loaded.
-  Drupal.behaviors.nodelist_node_blocks_hide_teaser = {
-    attach: function(context, settings) {
-      $('.field-type-text-long').toggleClass('element-hidden', true);
-    }
-  };
+
 
 
 })(jQuery);
